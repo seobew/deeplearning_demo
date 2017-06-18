0b9d46ab6af7dfdb60cbb4b72cd70be2e4e26ae5
KimISeob <seobew@gmail.com>
2017-06-19 03:26:18 +0900

    image save with white background

--- main/views.py
+++ main/views.py
@@ -1,6 +1,20 @@
+from django.conf import settings
 from django.shortcuts import render
+import base64
+from io import StringIO
+import PIL.Image
+from PIL import Image
+from base64 import decodebytes
+import re
 
 # Create your views here.
 
 def main(request):
+    if request.is_ajax():
+        imgstr = re.search(r'base64,(.*)', request.POST['img']).group(1)
+        imgdata = base64.b64decode(imgstr)
+        filename = 'test.png'
+        with open(filename, 'wb') as f:
+            f.write(imgdata)
+
     return render(request, 'main.html', )
