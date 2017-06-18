ffdbc43545bf1746bd21ff310b66d7c551bae08d
KimISeob <seobew@gmail.com>
2017-06-19 05:07:50 +0900

    copy pix2pix tensorflow part

--- main/views.py
+++ main/views.py
@@ -6,15 +6,25 @@ import PIL.Image
 from PIL import Image
 from base64 import decodebytes
 import re
+import os
+from subprocess import call
 
 # Create your views here.
 
 def main(request):
+    # call(["python ", "./tensorflow_part/tf.py"])
+    # cmd = 'python ./tensorflow_part/tf.py'
+    # cmd = 'python ./tensorflow_part/pix2pix.py --mode test --output_dir facades_test --input_dir facades/val --checkpoint facades_train'
+    cmd = 'python ./tensorflow_part/pix2pix.py --mode test --output_dir chicken_test --input_dir chicken/val --checkpoint chicken_train'
+    os.system(cmd)
     if request.is_ajax():
         imgstr = re.search(r'base64,(.*)', request.POST['img']).group(1)
         imgdata = base64.b64decode(imgstr)
         filename = 'test.png'
         with open(filename, 'wb') as f:
             f.write(imgdata)
+
+        # exec(open("./tensorflow_part/tf.py").read())
+
 
     return render(request, 'main.html', )
